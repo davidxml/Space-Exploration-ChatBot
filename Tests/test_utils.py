f0d36b87cd644af24ef37e4ae877e365d5efc6e2
davidxml <pycodegenius@gmail.com>
2025-09-22 21:57:11 +0100

    Added the test_utils file to test new and updated functions added to utilities

---
+++ Tests/test_utils.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from Backend.utils import preprocess_text
+
+
+
+# Example user inputs to test preprocessing
+
+sentences = ["The astronauts are runnning quicky!", "Spaceships flew faster than meteors.",
+             "He is running. Running is his hobby."]
+
+for s in sentences:
+    print(f"Original: {s}")
+    print(f"Processed: {preprocess_text(s)}")
+    print("-" * 40)
